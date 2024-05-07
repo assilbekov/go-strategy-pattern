8103af0fc0a8b574e64c9f11cc0a71800325fbf3
assilbekov <askhat.assilbekov@gmail.com>
2024-05-07 16:30:51 -0300

    implement strategies

--- main.go
+++ main.go
@@ -1,1 +1,21 @@
-package strategy
+package main
+
+import "strategy/pkg"
+
+var (
+	start      = 10
+	end        = 200
+	strategies = []pkg.Strategy{
+		&pkg.PublicStrategy{},
+		&pkg.RouteStrategy{},
+		&pkg.WalkStrategy{},
+	}
+)
+
+func main() {
+	navigator := &pkg.Navigator{}
+	for _, s := range strategies {
+		navigator.SetStrategy(s)
+		navigator.Route(start, end)
+	}
+}
