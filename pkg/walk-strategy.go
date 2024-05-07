6174389855b7101d9d85f38c907d02dd65519a73
assilbekov <askhat.assilbekov@gmail.com>
2024-05-07 16:28:56 -0300

    add WalkStrategy

--- pkg/walk-strategy.go
+++ pkg/walk-strategy.go
@@ -1,1 +1,17 @@
 package pkg
+
+import "fmt"
+
+type WalkStrategy struct {
+	//
+}
+
+func (w *WalkStrategy) Route(startPoint, endPoint int) {
+	avSpeed := 5
+	trafficJam := 1
+	totalDistance := endPoint - startPoint
+	totalTime := totalDistance * trafficJam / avSpeed
+
+	fmt.Printf("Road A: [%d] to Road B: [%d], Traffic Jam: [%d], Average Speed: [%d]\n", startPoint, endPoint, trafficJam, avSpeed)
+	fmt.Println("Total time: ", totalTime)
+}
